f684161fcb11bfc00b656727070f414531595166
Kayton <kaytoncwc@gmail.com>
2024-09-08 03:51:54 +0800

    encoding: undo the use of the helper fn as i was too naive

--- src/transport/encoding.rs
+++ src/transport/encoding.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-use rust_distributed_file::read_all_from_stream;
+// use rust_distributed_file::read_all_from_stream;
 
 use super::message::Message;
 
@@ -13,13 +13,13 @@ pub struct DefaultDecoder {}
 impl Decoder for DefaultDecoder {
     fn decode(&self, r: &mut dyn io::Read, msg: &mut Message) -> Result<(), io::Error> {
         // FIXME: it is not guaranteed that we will read all the bytes
-        // let mut buf = vec![0; 1024];
-        // let n = r.read(&mut buf).unwrap();
-        // println!("[Decoder] Read {} b1ytes", n);
-        // msg.payload = buf[..n].to_vec();
-        let buf = read_all_from_stream(r).unwrap();
-        msg.payload = buf;
-        
+        let mut buf = vec![0; 1024];
+        let n = r.read(&mut buf).unwrap();
+        println!("[Decoder] Read {} b1ytes", n);
+        msg.payload = buf[..n].to_vec();
+        // let buf = read_all_from_stream(r).unwrap();
+        // msg.payload = buf;
+
         Ok(())
     }
 }
